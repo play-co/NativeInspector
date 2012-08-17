c8ff0b2a70d012bcd0696d95c0f3813d381df843
Christopher A. Taylor <chris@gameclosure.com>
2012-08-16 18:12:30 -0700

    Add bind shim

--- front-end/utilities.js
+++ front-end/utilities.js
@@ -27,6 +27,30 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// Added this bind shim for older versions of Safari -cat
+if (typeof Function.prototype.bind != 'function') {
+  Function.prototype.bind = (function () {
+    var slice = Array.prototype.slice;
+    return function (thisArg) {
+      var target = this, boundArgs = slice.call(arguments, 1);
+
+      if (typeof target != 'function') throw new TypeError();
+
+      function bound() {
+	var args = boundArgs.concat(slice.call(arguments));
+	target.apply(this instanceof bound ? this : thisArg, args);
+      }
+
+      bound.prototype = (function F(proto) {
+          proto && (F.prototype = proto);
+          if (!(this instanceof F)) return new F;          
+	})(target.prototype);
+
+      return bound;
+    };
+  })();
+}
+
 Object.isEmpty = function(obj)
 {
     for (var i in obj)
